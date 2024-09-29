39ce3bec39ad60d2dc717928637b0a8851eb594d
nelvinastres <151507789+nelvinastres@users.noreply.github.com>
2024-09-29 17:44:25 +0700

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WearHouseMiniProject.Models;
+
+namespace WearHouseMiniProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<UserData> UserData {  get; set; }
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ Data/EditCategory.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WearHouseMiniProject.Data
+{
+    public class EditCategory
+    {
+        //public string UserID { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+    }
+}
